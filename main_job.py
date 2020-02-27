9bcadef72cba76a6979dcb8b8e95362771537863
Victor Santiago <vsantiago113sec@gmail.com>
2020-02-26 20:38:50 -0600

    Update
    Now Nornir is going to create a failed_hosts.yaml file of the failed hosts and is not going to add the failed hosts to the extended_nornir.log file. Is also going to append to the extended_nornir.log file instead of creating a new one every time. The playbook will run the failed_hosts.yaml file as the inventory if it exists if not is going to run the hosts.yaml but if you want to run the hosts.yaml and not the failed_hosts.yaml then rename or delete the failed_hosts.yaml file. The reason for this change is to be able to run the playbook again only on the failed hosts and append the successful ones in the extended_nornir.log to easily keep track of everything.

--- main_job.py
+++ main_job.py
@@ -7,6 +7,11 @@ from nornir.plugins.functions.text import print_title
 
 os.mkdir('logs') if not os.path.isdir('logs') else None
 
+if os.path.isfile('inventory/failed_hosts.yaml'):
+    inventory = 'inventory/failed_hosts.yaml'
+else:
+    'inventory/hosts.yaml'
+
 nr = InitNornir(config_file='config.yaml', dry_run=False,
                 logging={'enabled': True, 'level': 'debug', 'to_console': True, 'file': 'logs/nornir.log',
                          'format': '%(asctime)s - %(name)s - %(levelname)s - %(funcName)s() - %(message)s'})
@@ -26,13 +31,11 @@ def basic_configuration(task):
                  template='base.j2',
                  path=f'templates/{task.host.platform}',
                  severity_level=logging.DEBUG)
-    # Save the compiled configuration into a host variable
-    task.host['config'] = r.result
 
     # Deploy that configuration to the device using Netmiko
     task.run(task=netmiko_send_config,
              name='Loading Configuration on the Device',
-             config_commands=task.host['config'].splitlines(),
+             config_commands=r.result.splitlines(),
              severity_level=logging.INFO)
 
     # Save that configuration to the device using Netmiko
@@ -53,13 +56,27 @@ print_title('Playbook to setup basic configs')
 hosts = nr.filter(filter_func=custom_filter)
 multi_result = hosts.run(task=basic_configuration)
 
-with open('logs/extended_nornir.log', 'w') as f:
-    for device, result, in multi_result.items():
-        if result.failed:
-            f.write(f' Name: {device}, IP Address: {result[0].host.hostname} - FAILED! '.center(80, '*') + '\n')
+failed_hosts_inv = dict()
+with open('logs/extended_nornir.log', 'a' if os.path.isfile('logs/extended_nornir.log') else 'w') as f:
+    for device_name, result, in multi_result.items():
+        if not result.failed:
+            f.write(f' Name: {device_name}, IP Address: {result[0].host.hostname} - SUCCESS! '.center(80, '*') + '\n')
+            for chore in result:
+                if chore.result and chore.name not in ['Loading Jinja Template']:
+                    f.write(str(chore.result) + '\n')
+            f.write(f'{"~" * 80}\n')
         else:
-            f.write(f' Name: {device}, IP Address: {result[0].host.hostname} - SUCCESS! '.center(80, '*') + '\n')
-            for stdout in result[2:]:
-                if stdout.result:
-                    f.write(str(stdout) + '\n')
-        f.write(f'{"~" * 80}\n')
+            failed_host = result[0].host
+            failed_hosts_inv[failed_host.name] = {'hostname': failed_host.hostname,
+                                                  'platform': failed_host.platform}
+            if failed_host.groups:
+                failed_hosts_inv[failed_host.name].update({'groups': list(failed_host.groups)})
+            if failed_host.data:
+                failed_hosts_inv[failed_host.name].update({'data': dict(failed_host.data)})
+
+if failed_hosts_inv:
+    with open('inventory/failed_hosts.yaml', 'w') as f:
+        yaml.dump(failed_hosts_inv, f)
+else:
+    if os.path.isfile('inventory/failed_hosts.yaml'):
+        os.remove('inventory/failed_hosts.yaml')
